dbbbb6c3bae46d5887f92c24b12588ab02a67868
ankolesn <anast.kolesnikova@mail.ru>
2023-04-23 12:09:45 +0300

    init

--- main.cpp
+++ main.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 
-int main() {
-//    ifstream file("file");
-//    if (!file.is_open()) {
-//        cout << "Error: cannot open input file " << argv[1] << endl;
-//        return 1;
-//    }
+int main(int argc, char* argv[]) {
+    ifstream file("file");
+    if (!file.is_open()) {
+        cout << "Error: cannot open input file " << argv[1] << endl;
+        return 1;
+    }
 
     Processing_Event p_e;
 
     try {
-        p_e.parse_file("file");
+        p_e.parse_file(argv[1]);
     }
     catch (const string& error_message) {
         cout << error_message << endl;
         ::exit(0);
     }
     p_e.print();
-//    file.close();
+    file.close();
 }
